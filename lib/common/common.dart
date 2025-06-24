a5eb766c87904db655e90b83b3526fe386b6854f
ashishtandon2000 <tandonashish15@gmail.com>
2025-06-24 13:06:45 +0530

    added get map safely functionality

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -1,6 +1,7 @@
 library common;
 
 import 'package:flutter/material.dart';
+import 'package:macro_diary/common/functions/get_map_safely.dart';
 
 part "functions/prints.dart";
 part 'widgets/loader.dart';
@@ -9,6 +10,9 @@ part 'widgets/loader.dart';
 
 class Util {
   const Util();
+
+  // f- functions
+  static const fGetMapSafely = getMapSafely;
 
   // g- group
   static const print = _appPrints();
---
+++ lib/common/functions/get_map_safely.dart
@@ -0,0 +1,24 @@
+/// Can handle extracting (string, int, bool, list & map) from a map, returning datatype's 0 value in case of any error
+dynamic getMapSafely<valueType>({required Map data, required String key}) {
+  try {
+    valueType val = data[key] as valueType;
+    return val;
+  } catch (err) {
+    switch (valueType) {
+      case String:
+        return "";
+      case int:
+        return 0;
+      case double:
+        return 0.0;
+      case bool:
+        return false;
+      case List:
+        return [];
+      case Map:
+        return {};
+      default:
+        return null;
+    }
+  }
+}
