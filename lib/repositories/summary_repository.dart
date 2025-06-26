685d0ce924f3cc24845248d29055db79ae4d3c59
ashishtandon2000 <tandonashish15@gmail.com>
2025-06-26 12:34:38 +0530

    added functionality of summary

--- lib/repositories/summary_repository.dart
+++ lib/repositories/summary_repository.dart
@@ -1,20 +1,62 @@
 
 import 'package:macro_diary/models/macros.dart';
+import 'package:macro_diary/repositories/db.dart';
+import 'package:macro_diary/repositories/util.dart';
 
 abstract class ISummaryRepository {
 
-  Future<Macros> getCurrentSummary();
+  Macros getCurrentSummary();
+  Future<bool> addToSummary(Macros macros); // adds to existing summary
+  Future<bool> overrideSummary(Macros macros); // overrides summary with new macros
 }
 
 class SummaryRepositoryImpl implements ISummaryRepository {
 
+  final _uniqueKey = "food_summary";
+  key(String id) => "${_uniqueKey}_$id";
+
+  String _getDailyKey() {
+    final now = DateTime.now();
+    final dayKey = "${now.year}-${now.month}-${now.day}";
+    return dayKey; // Example: "2025-6-26"
+  }
+
   @override
-  Future<Macros> getCurrentSummary() async {
+  Macros getCurrentSummary() {
+
+    final todayKey = key(_getDailyKey());
+
+    final data =  DB().instance.getString(todayKey);
+
+    if(data!=null){
+     return Codec.decode<Macros>(data);
+    }
     return Macros(
-      protein: 0,
-      carbs: 0,
-      fats: 0,
-      calories: 0,
+        calories: 0,
+        protein: 0,
+        carbs: 0,
+        fats: 0
     );
+  }
+
+  @override
+  Future<bool> addToSummary(Macros macros) async {
+    final todayKey = key(_getDailyKey());
+
+    final currentSummary = getCurrentSummary();
+    currentSummary.add(macros);
+
+    final json = Codec.encode(currentSummary);
+
+    return await DB().instance.setString(todayKey, json);
+  }
+
+  @override
+  Future<bool> overrideSummary(Macros macros) async {
+    final todayKey = key(_getDailyKey());
+
+    final json = Codec.encode(macros);
+
+    return await DB().instance.setString(todayKey, json);
   }
 }
