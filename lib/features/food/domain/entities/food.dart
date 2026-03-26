601b0f8d29732347d325fd42d8e01e20dc90171f
ashishtandon2000 <tandonashish15@gmail.com>
2026-03-26 19:19:14 +0530

    add food as domain entity

--- lib/features/food/domain/entities/food.dart
+++ lib/features/food/domain/entities/food.dart
@@ -1,4 +1,34 @@
+import 'package:macro_diary/core/domain/entities/macros.dart';
 
-class FoodItem{
+enum MeasureUnit { gram, milliliter, piece }
 
+class Food {
+  final String id;
+  final String name;
+  final Macros macros;
+  final MeasureUnit unit;
+
+  const Food({
+    required this.id,
+    required this.name,
+    required this.macros,
+    required this.unit,
+  });
+
+  Food copyWith({
+    String? id,
+    String? name,
+    Macros? macros,
+    MeasureUnit? unit,
+  }) {
+    return Food(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      macros: macros ?? this.macros,
+      unit: unit ?? this.unit,
+    );
+  }
+
+  @override
+  String toString() => 'Food(id: $id, name: $name)';
 }
