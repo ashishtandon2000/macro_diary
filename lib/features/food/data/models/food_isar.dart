023db74b5168e2463e2bc296c6f6344dd51e4c4f
ashishtandon2000 <tandonashish15@gmail.com>
2026-04-18 19:29:35 +0530

    add mapping methods to isar food model

--- lib/features/food/data/models/food_isar.dart
+++ lib/features/food/data/models/food_isar.dart
@@ -1,4 +1,6 @@
 import 'package:isar/isar.dart';
+import 'package:macro_diary/core/domain/entities/macros.dart';
+import 'package:macro_diary/features/food/domain/entities/food.dart';
 
 part 'food_isar.g.dart';
 
@@ -16,4 +18,31 @@ class FoodIsar {
   String unit = 'gram';
 
   String? externalId;
+
+  Food toEntity() {
+    return Food(
+      id: id.toString(),
+      name: name,
+      macros: Macros(
+        calories: calories,
+        protein: protein,
+        carbs: carbs,
+        fats: fats,
+      ),
+      unit: MeasureUnit.values.firstWhere((e) => e.name == unit),
+      externalId: externalId
+
+    );
+  }
+
+  static FoodIsar fromEntity(Food entity) {
+    return FoodIsar()
+      ..name = entity.name
+      ..calories = entity.macros.calories
+      ..protein = entity.macros.protein
+      ..carbs = entity.macros.carbs
+      ..fats = entity.macros.fats
+      ..unit = entity.unit.name
+      ..externalId = entity.externalId;
+  }
 }
