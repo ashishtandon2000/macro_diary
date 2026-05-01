e05e63b9794fb4bf8868d66eb1fd900e623d4da8
ashishtandon2000 <tandonashish15@gmail.com>
2026-05-01 13:53:03 +0530

    Create base for data layer

---
+++ lib/features/foodServing/data/repositories/food_serving_repository_impl.dart
@@ -0,0 +1,36 @@
+
+import 'package:macro_diary/features/foodServing/domain/entities/food_serving.dart';
+import 'package:macro_diary/features/foodServing/domain/repositories/food_serving_repository.dart';
+
+class FoodServingRepositoryImpl extends FoodServingRepository{
+
+  @override
+  Future<void> addServing(FoodServing serving) {
+    // TODO: implement addServing
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateServing(FoodServing serving) {
+    // TODO: implement updateServing
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteServing(String servingId) {
+    // TODO: implement deleteServing
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<FoodServing> getServingById() {
+    // TODO: implement getServingById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<FoodServing>> getAllServings() {
+    // TODO: implement getAllServings
+    throw UnimplementedError();
+  }
+}
