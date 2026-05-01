4cbe7455b4316877ea89dd2a6ee9c82de87a6277
ashishtandon2000 <tandonashish15@gmail.com>
2026-05-01 07:44:52 +0530

    Create domain repo contract

---
+++ lib/features/foodServing/domain/repositories/food_serving_repository.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:macro_diary/features/foodServing/domain/entities/food_serving.dart';
+
+abstract class FoodServingRepository {
+
+  Future<List<FoodServing>> getAllServings();
+  Future<FoodServing> getServingById();
+
+  Future<void> addServing(FoodServing serving);
+  Future<void> updateServing(FoodServing serving);
+  Future<void> deleteServing(String servingId);
+}
