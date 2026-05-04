83561b136d4faec9af5b34309138e463778a1910
ashishtandon2000 <tandonashish15@gmail.com>
2026-05-04 12:36:24 +0530

    Create db service for food serving

--- lib/features/foodServing/data/services/food_serving_local_service.dart
+++ lib/features/foodServing/data/services/food_serving_local_service.dart
@@ -1,5 +1,30 @@
-
+import 'package:isar/isar.dart';
+import 'package:macro_diary/features/foodServing/data/models/food_serving_isar.dart';
 
 class FoodServingLocalService {
 
+  final Isar isar;
+
+  FoodServingLocalService({required this.isar});
+
+  Future<void> addServing(FoodServingIsar serving)async{
+    await isar.writeTxn(()async{
+      isar.foodServingIsars.put(serving);
+    });
+  }
+
+  Future<void> deleteServing(int id)async{
+      await isar.writeTxn(()async{
+        isar.foodServingIsars.delete(id);
+      });
+  }
+
+
+  Future<List<FoodServingIsar>> getAllServings()async{
+    return await isar.foodServingIsars.where().findAll();
+  }
+
+  Future<FoodServingIsar?> getServingById(int id)async{
+    return await isar.foodServingIsars.get(id);
+  }
 }
