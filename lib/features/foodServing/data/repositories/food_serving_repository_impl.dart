2c52e5c4a888d6ee02cdd15b925218296a6652d3
ashishtandon2000 <tandonashish15@gmail.com>
2026-05-05 08:00:32 +0530

    Implement service methods in repo

--- lib/features/foodServing/data/repositories/food_serving_repository_impl.dart
+++ lib/features/foodServing/data/repositories/food_serving_repository_impl.dart
@@ -1,5 +1,8 @@
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:macro_diary/core/errors/exceptions.dart';
+import 'package:macro_diary/core/errors/failures.dart';
+import 'package:macro_diary/features/foodServing/data/models/food_serving_isar.dart';
 import 'package:macro_diary/features/foodServing/data/services/food_serving_local_service.dart';
 import 'package:macro_diary/features/foodServing/domain/entities/food_serving.dart';
 import 'package:macro_diary/features/foodServing/domain/repositories/food_serving_repository.dart';
@@ -20,32 +23,53 @@ class FoodServingRepositoryImpl extends FoodServingRepository{
   FoodServingRepositoryImpl({required this.servingLocalService});
 
   @override
-  Future<void> addServing(FoodServing serving) {
-    // TODO: implement addServing
-    throw UnimplementedError();
+  Future<void> addServing(FoodServing serving) async{
+    try{
+      final model = FoodServingIsar.fromEntity(serving);
+      await servingLocalService.addServing(model);
+    }catch(_){
+      throw const CacheFailure("Failed to save serving");
+    }
   }
 
   @override
-  Future<void> updateServing(FoodServing serving) {
-    // TODO: implement updateServing
-    throw UnimplementedError();
+  Future<void> updateServing(FoodServing serving) async{
+    try{
+      final model = FoodServingIsar.fromEntityWithId(serving);
+      await servingLocalService.addServing(model);
+    }catch(_){
+      throw const CacheFailure("Failed to update serving");
+    }
   }
 
   @override
-  Future<void> deleteServing(String servingId) {
-    // TODO: implement deleteServing
-    throw UnimplementedError();
+  Future<void> deleteServing(String servingId) async{
+    try{
+      await servingLocalService.deleteServing(int.parse(servingId));
+    }catch(_){
+      throw const CacheFailure("Failed to delete serving");
+    }
   }
 
   @override
-  Future<FoodServing> getServingById() {
-    // TODO: implement getServingById
-    throw UnimplementedError();
+  Future<FoodServing?> getServingById(String servingId) async{
+    try{
+      final serving = await servingLocalService.getServingById(int.parse(servingId));
+      return serving?.toEntity();
+    }catch(_){
+      throw const CacheFailure("Failed to fetch serving");
+    }
   }
 
   @override
-  Future<List<FoodServing>> getAllServings() {
-    // TODO: implement getAllServings
-    throw UnimplementedError();
+  Future<List<FoodServing>> getAllServings() async {
+    try{
+      final servings = await servingLocalService.getAllServings();
+      return servings.map((s)=>s.toEntity()).toList();
+    } on ParsingException {
+      throw const ServerFailure("Invalid data from server");
+    } on ServerException {
+      throw const ServerFailure("Failed to fetch servings");
+    }
   }
 }
